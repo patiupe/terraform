0d9277c2f3c92d8f4ebe48eea942a09a82f458fb
Upendra Patil <upatil@jhancock.com>
2024-10-13 17:11:06 -0400

    test

--- main.tf
+++ main.tf
@@ -44,7 +44,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
   listeners = {
